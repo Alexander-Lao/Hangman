a9f6d409422c6bac06df611c07b8bb6d7fd556af
Alexander-Lao <alexander.lao@student.tdsb.on.ca>
2022-02-09 22:45:48 -0500

    added random word generator

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 Hopefully you guys can edit this file. If you can, feel free to make edits to the "to do" list
 
-To do:
+Done (Maybe can be optimized):
 Add word choice randomizer
   - Some sort of dictionary
   - Maybe an API
 
+To do:
 Add game elements
   - Lives
     - Repeating the same letter should not decrease lives
--- hang.cpp
+++ hang.cpp
@@ -1,35 +1,60 @@
 #include <iostream>
+#include <fstream> //file reader
 using namespace std;
 
-string word = "dictionary";
+string word;
+
+int rights = 0;
+string guess;
+string temp;
+
 
 int main() {
+    //Start of random word generator (from the 1000 words in dictionary.txt)
+    srand(time(0));
+    int ran = (rand() %1000 * rand() %1000) %1000 +1; //random value from 1 to 1000
 
+    ifstream dictionary; 
+
+    dictionary.open("dictionary.txt"); //there are a couple of sketchy words in here
+    if(dictionary.is_open()){
+        string x;
+
+        for (int i = 0 ; i < ran ; i++){ 
+	        dictionary >> x;
+        } 
+        word = x;
+        dictionary.close();
+    }
     int length = word.length();
+    //End of random word generator
+
+    cout << word +"\n"; //Prints out the correct answer, DELETE AT END
+
     string answer[length];
     string response[length];
 
+    //Setting the correct answer and current user response
     for(int i = 0; i < length; i++){
-        answer[i].push_back(word[i]); //Change char into string to add space behind
+        answer[i].push_back(word[i]); 
         answer[i] = answer[i] + " ";
         response[i] = "_ ";
     }
 
-    int rights = 0;
-    string guess;
-    string temp;
-
+    //user interaction loop
     while(rights < length){
 
+        //prints out current response with filled in guesses
         for(int i = 0; i < length; i++){
             cout << response[i];
         }
+
         cout << "\n Enter a letter:";
         cin >> guess;
         temp = "";
         temp.push_back(guess[0]);
 
-
+        //check if guess is right, if it is, add letter to response
         for(int i = 0; i < length; i++){
             if(answer[i] ==  temp + " "){
                 response[i] = answer[i];
